040d602c3a84b5dce34df8bda24c8622a54efd23
Joshua Alcazar <alcajosh@outlook.com>
2025-11-13 14:09:45 +0000

    add new files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sandwich_shopf/main.dart';
-
-import 'package:week_1_worksheet/main.dart';
+import 'package:sandwich_shop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
